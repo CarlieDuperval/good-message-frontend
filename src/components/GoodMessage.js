684f8076aeacdbcb574235ef8f17f601557d6008
Carlie Duperval <carlierichell@gmail.com>
2022-05-29 17:53:06 -0400

    Creaetd unit files componentand Connect to database

---
+++ src/components/GoodMessage.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const GoodMessages = () => {
+  const [goodMessages, setGoodMessages] = useState("");
+  useEffect(() => {
+    fetch("https://good-message-backend.web.app/goodMessages")
+      .then((response) => response.json())
+      .then((data) => setGoodMessages(data))
+      .catch(console.error);
+  }, [goodMessages]);
+
+  return (
+    <>
+      <h1>Leave A Positive Message 🌞</h1>
+      {!goodMessages ? (
+        <h2>Loading...</h2>
+      ) : (
+        <>
+          <h2>Good Messages</h2>
+          {goodMessages.map((goodMessage) => {
+            return (
+              <p key={goodMessage.id}>
+                 {goodMessage.message}
+              </p>
+            );
+          })}
+        </>
+      )}
+    </>
+  );
+};
+
+export default GoodMessages;
